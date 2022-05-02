873a27695f6831b74b56dcf889ddd4221b3e1c9d
esatiz <104695316+esatiz@users.noreply.github.com>
2022-05-02 23:14:36 +0300

    Add files via upload

---
+++ Selection Sort.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i, j, min, *dizi, sayi;
+	
+	printf("Kac tane sayi girmek istiyorsunuz? > ");
+	scanf("%d", &sayi);
+	
+	printf("Lutfen diziyi giriniz\n\n");
+	
+	dizi = (int*) malloc(sizeof(int) * sayi); // RAM'de (4 * sayi) kadar byte yer ayrilir
+	
+	for (i = 0; i < sayi; i++) // DIZIYI DOLDURMA
+	{
+		scanf("%d", &dizi[i]);
+	}
+	
+	// OPTIMIZED SELECTION SORT
+	
+	for (i = 0; i < sayi - 1; i++)
+	{
+		min = i;
+		
+		for (j = min; j < sayi; j++)
+		{
+			if (dizi[min] > dizi[j])
+			{
+				min = j;
+			}
+		}
+		
+		// TAKAS
+		
+		if (dizi[i] != dizi[min]) // Sayýlar aynýysa XOR kapýsýnýn sonucu 0 olur! Sayýlarýmýzýn kaybolmasýný istemeyiz!
+		{
+			dizi[i] ^= dizi[min];
+			dizi[min] ^= dizi[i];
+			dizi[i] ^= dizi[min];
+		}
+		
+		// TAKAS
+	}
+	
+	// OPTIMIZED SELECTION SORT
+	
+	printf("\n\nDizinin yeni hali : ");
+	
+	for (i = 0; i < sayi; i++)
+	{
+		printf("%d ", dizi[i]);
+	}
+	
+	free(dizi); // MEMORY LEAK KORUMASI
+	
+	getch();
+	return 0;
+}
